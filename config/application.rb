6ed3852481af1a07d53eac6aa6a252afdc4f34d5
Heitor Carneiro <heitorgcarneiro@gmail.com>
2018-02-15 21:48:46 -0200

    Adding controllers

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,6 @@ module Onenote
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.web_console.whitelisted_ips = '192.168.99.1'
   end
 end
